9b67e03de72f35e390e9f53e82e5ff820c8e5a4d
leichunhong <leichunhong@thinkjoy.com>
2020-07-27 00:02:15 +0800

    第二周作业！

--- Week01/leichunhong/java/MergeTwoLists.java
+++ Week01/leichunhong/java/MergeTwoLists.java
@@ -4,7 +4,7 @@ import Week01.leichunhong.dto.ListNode;
 
 /**
  * 〈功能简述〉<br>
- * 〈〉
+ * 〈〉21. 合并两个有序链表
  * 合并有序链表
  * 步骤：1 两个队列有一个为空就为空
  *      2.当l1的val<l2的val
@@ -72,6 +72,35 @@ public class MergeTwoLists {
         }
 
     }
+    /**
+    * @auther  leichunhong
+    * @desc 第五周复习  多少个日日夜夜孤独的我再黑夜中默默敲着代码，写着属于自己的宁静
+    * @date  2020-07-07 22:34
+    * @param  [l1, l2]
+    * @return  Week01.leichunhong.dto.ListNode
+    */
+    public ListNode mergeTwoLists1List2(ListNode l1, ListNode l2) {
+
+        if(l1==null){
+            return l2;
+
+        }
+        if(l2==null){
+            return l1;
+
+        }
+        if(l1.val<l2.val){
+            l1.next=mergeTwoLists1List2(l1.next,l2);
+            return l1;
+
+        }else{
+            l2.next=mergeTwoLists1List2(l1,l2.next);
+            return l2;
+        }
+    }
+
+
+
 
 
 
